af5e4e6b197d9e2ac5a6e8a6aa37c06957935275
VonStruddle <quentin.durantay@gmail.com>
2016-10-13 15:17:36 +0200

    Create AddTodo component and tests for it

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 
 var TodoList = require('TodoList');
+var AddTodo = require('AddTodo');
 
 var TodoApp = React.createClass({
   getInitialState: function() {
@@ -30,12 +31,17 @@ var TodoApp = React.createClass({
     };
   },
 
+  handleNewTodo: function(text) {
+    alert('New todo: ' + text);
+  },
+
   render: function() {
     var {todos} = this.state;
 
     return (
       <div>
         <TodoList todos={todos}/>
+        <AddTodo onAddTodo={this.handleNewTodo}/>
       </div>
     );
   }
