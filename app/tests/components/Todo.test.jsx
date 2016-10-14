aad45eaba566cdc9373d33cd7013abe51d2a9ff1
VonStruddle <quentin.durantay@gmail.com>
2016-10-14 14:39:38 +0200

    Add toggle testing

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -3,11 +3,31 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+var uuid = require('node-uuid');
 
 var Todo = require('Todo');
 
 describe('Todo', () => {
   it('should exist', () => {
     expect(Todo).toExist;
+  });
+
+  it('should call onToggle prop with id on click', () => {
+    var id = uuid.v4();
+    var todoData = {
+      id: id,
+      text: 'Hug mother',
+      completed: false
+    }
+
+    var spy = expect.createSpy();
+    var todo = TestUtils.renderIntoDocument(<Todo {...todoData} onToggle={spy}/>);
+
+    expect(spy).toNotHaveBeenCalled();
+
+    var $el = $(ReactDOM.findDOMNode(todo));
+    TestUtils.Simulate.click($el[0]);
+    
+    expect(spy).toHaveBeenCalledWith(id);
   });
 });
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -3,6 +3,7 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
+var uuid = require('node-uuid');
 
 var TodoApp = require('TodoApp');
 
@@ -20,5 +21,22 @@ describe('TodoApp', () => {
 
     expect(todoApp.state.todos[0].text).toBe(todoText);
     expect(todoApp.state.todos.length).toBe(1);
+  });
+
+  it('should update completed value when handleToggle called', () => {
+    var id = uuid.v4();
+    var todoData = {
+      id: id,
+      text: 'Hug girlfriend',
+      completed: false
+    }
+
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(false);
+
+    todoApp.handleToggle(id);
+    expect(todoApp.state.todos[0].completed).toBe(true);
   });
 });
