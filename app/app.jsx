3aac5e15816fd671c6226ab675079a51dc5e573b
VonStruddle <quentin.durantay@gmail.com>
2016-10-28 18:57:25 +0200

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,16 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var firebase = require("firebase");
 
 var TodoApp = require('TodoApp');
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+	console.log('New state:', store.getState());
+});
+
+store.dispatch(actions.addTodo('Do the dishes'));
+store.dispatch(actions.setSearchText('dish'));
+store.dispatch(actions.toggleShowCompleted());
 
 // Load Foundation
 $(document).foundation();
